e08240106f58cf4425ed5fe3a15fd7bf55db4250
Hustin Jeffers <hj.paul7@gmail.com>
2020-08-30 15:08:54 -0400

    Add project files.

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,59 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userID;
+
+        public NoteService(Guid userId)
+        {
+            _userID = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userID,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Notes
+                    .Where(e => e.OwnerId == _userID)
+                    .Select(
+                        e =>
+                        new NoteListItem
+                        {
+                            NoteId = e.NoteId,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
